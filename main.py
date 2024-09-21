5b14226329f2e5b6e3dda64407cfe1f0a99e808b
Yitong0 <141650628+Yitong0@users.noreply.github.com>
2024-09-21 18:50:19 +0200

    Fix PEP 8 style issues (whitespace after commas)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # Create a list of numbers from 1 to 10
-numbers = list([1,2,3,4,5,6,7,8,9,10])
+numbers = list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 # Create a NumPy array from the list
 array = np.array(numbers)
